9f509cf29bafdb38f9a6e98a13ac0c0add47792d
noblet <appiahpatricknoblet@gmail.com>
2024-07-27 20:17:00 +0100

    create report object function

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,25 @@
+/**
+ * Creates a report object that includes a list of employees and a method to
+ * get the number of departments.
+ *
+ * @param {Object} employeesList - An object where each key is a department name and
+ *                                 the value is an array of employee names.
+ * @returns {Object} An object with the following properties:
+ *   - `allEmployees`: An object containing the original employeesList.
+ *   - `getNumberOfDepartments`: A method that returns the number of departments in
+ *      the employeesList.
+ */
+
+export default function createReportObject(employeesList) {
+  const employees = {
+    allEmployees: {
+      ...employeesList,
+    },
+
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+
+  return employees;
+}
