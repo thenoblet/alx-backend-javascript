763a72c1f42d8125d89f2a6ea47c41b779e6ca61
noblet <appiahpatricknoblet@gmail.com>
2024-07-27 19:09:39 +0100

    ADDED: function that utilises computed property names to set property names of the object returned

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,8 @@
 /**
  * Creates an object where the key is the department name and the value is the list of employees.
+ * => Uses Computed Property names to dynamically set the name of the property in the object.
+ *        **useful when you need to create an object with property names that are
+ *          not known until runtime.
  *
  * @param {string} departmentName - The name of the department.
  * @param {Array<string>} employees - An array of employee names in the department.
