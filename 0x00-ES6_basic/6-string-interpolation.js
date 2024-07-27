429b69c7b58de62451cdc074366174b08e94c267
noblet <appiahpatricknoblet@gmail.com>
2024-07-27 15:23:58 +0100

    ADDED: A function that uses template literals

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,16 @@
+/**
+ * Returns a description of San Francisco's economic statistics.
+ *
+ * @returns {string} A string describing San Francisco's income, GDP, and GDP per capita as of specified years.
+ */
+
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+}
