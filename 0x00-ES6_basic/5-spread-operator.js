044cb8b87947b0bc75aa0fed972ee4993339ce2d
noblet <appiahpatricknoblet@gmail.com>
2024-07-27 04:06:04 +0100

    added a function that utilises the spread operation

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,13 @@
+/**
+ * Concatenates two arrays and a string into a single array.
+ *
+ * @param {Array} array1 - The first array.
+ * @param {Array} array2 - The second array.
+ * @param {string} string - The string to be split and concatenated.
+ * @returns {Array} The concatenated array containing elements from both
+ *                  arrays and the characters of the string.
+ */
+
 export default function concatArrays(array1, array2, string) {
-	return [...array1, ...array2, ...string]
+  return [...array1, ...array2, ...string];
 }
