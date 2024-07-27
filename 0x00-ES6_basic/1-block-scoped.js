63b0bf0d1439aa266a80ff583ba47eacc52b95e1
noblet <appiahpatricknoblet@gmail.com>
2024-07-27 03:01:34 +0100

    added block scoped function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
+/* eslint-disable */
+
+
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
   if (trueOrFalse) {
-	  let task = true;
-	  let task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
