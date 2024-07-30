b9ec1f677bfb4acc79425db8bdcad1378b93befd
noblet <appiahpatricknoblet@gmail.com>
2024-07-31 00:49:38 +0100

    Added a function that returns a list of students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,11 @@
 /**
  * Returns a list of student objects.
  *
- * @returns {Array<Object>} An array of student objects, each containing `id`, `firstName`, and `location` properties.
+ * @returns {Array<Object>} An array of student objects, each containing `id`, `firstName`, and
+ * `location` properties.
  */
 
-function getListStudents() {
+export default function getListStudents() {
   const studentsList = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
