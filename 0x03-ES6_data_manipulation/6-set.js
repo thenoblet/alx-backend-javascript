7505687386bd42eeea61f5497f7625bf840643b6
noblet <appiahpatricknoblet@gmail.com>
2024-07-31 03:54:00 +0100

    Added a function that creates a set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a Set from an array.
+ *
+ * @param {Array} array - The array to convert to a Set.
+ * @returns {Set} - A new Set containing the elements of the array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
