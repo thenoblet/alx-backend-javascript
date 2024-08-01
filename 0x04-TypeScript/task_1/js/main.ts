801adfe5a6b2b261aab02be0b11debad06def50a
noblet <appiahpatricknoblet@gmail.com>
2024-08-01 03:09:15 +0100

    ADDED a student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,31 @@ interface printTeacherFunction{
 let printTeacher: printTeacherFunction = (firstName, lastName): string => {
 	return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface studentClassConstructor{
+	firstName: string;
+	lastName: string;
+}
+
+interface studentClassInterface{
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+class studentClass implements studentClassInterface {
+	firstName: string;
+	lastName: string;
+
+	constructor({ firstName, lastName }: studentClassConstructor) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return "Currently working";
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
